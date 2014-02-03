656ee43c0a4b08f99f9d4c1da03f507ca00027be
Jesus Rocha <rocha.jesus@hotmail.com>
2014-02-03 02:09:10 -0800

    Removed two files

--- app-server.js
+++
@@ -1,8 +0,0 @@
-var app = require("./EchoClientServer");
-
-var options = {};
-
-options["port"] = app.util.getARGVs( process.argv, ['-p', '--port'], 'number');
-options["quiet"] = app.util.getARGVs( process.argv, ['-q', '--quiet'], 'bool');
-
-app.server.start( options );
